3cc02eef2d383e52a686e35708620c8c10486964
Stirling Hostetter <stirhoss@icloud.com>
2022-06-13 12:13:01 -0500

    Create AR for adult passengers

--- app/models/airline.rb
+++ app/models/airline.rb
@@ -1,3 +1,8 @@
 class Airline < ApplicationRecord
   has_many :flights
+  has_many :passengers, through: :flights
+
+  def airline_adult_passengers
+    passengers.where('age >= 18').distinct
+  end
 end

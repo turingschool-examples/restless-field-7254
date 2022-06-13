18e9e68d697152e8d15059f7a2ba22faec4e40e2
Colin Reinhart <colinbreinhartgmail.com>
2022-06-13 11:44:25 -0600

    Refactor 1

--- spec/features/flights/index_spec.rb
+++ spec/features/flights/index_spec.rb
@@ -20,19 +20,17 @@ RSpec.describe "Flights index page" do
     @fp1 = FlightPassenger.create!(passenger_id: "#{@bre.id}", flight_id: "#{@flight1.id}")
     @fp2 = FlightPassenger.create!(passenger_id: "#{@aab.id}", flight_id: "#{@flight2.id}")
     @fp3 = FlightPassenger.create!(passenger_id: "#{@bill.id}", flight_id: "#{@flight3.id}")
+
+      visit flights_path
   end
 
   it "shows all flight numbers" do
-    visit "/flights"
-
     expect(page).to have_content("7790")
     expect(page).to have_content("3940")
     expect(page).to have_content("9091")
   end
 
   it "shows the name of the airline associated with flight" do
-    visit "/flights"
-
     expect(page).to have_content("American")
     expect(page).to have_content("Delta")
     expect(page).to have_content("JetBlue")
@@ -42,26 +40,16 @@ RSpec.describe "Flights index page" do
   end
 
   it "shows names of all passengers on each flight" do
-    visit "/flights"
-
-    expect(page).to have_content("Brennan Lee Mulligan")
-    expect(page).to have_content("Aabria Iyengar")
-    expect(page).to have_content("Bill Seacaster")
-
+    within "#flight-#{@flight1.id}" do
+      expect(page).to have_content("Brennan Lee Mulligan")
+    end
+    within "#flight-#{@flight2.id}" do
+      expect(page).to have_content("Aabria Iyengar")
+    end
+    within "#flight-#{@flight3.id}" do
+      expect(page).to have_content("Bill Seacaster")
+    end
     expect(page).to_not have_content("Misty Moore")
-
-    # within "#flight-#{@flight1.id}" do
-    #   require "pry"; binding.pry
-    #   expect(page).to have_content("Brennan Lee Mulligan")
-    # end
-    # within "#flight-#{@flight2.id}" do
-    #   require "pry"; binding.pry
-    #   expect(page).to have_content("Aabria Iyengar")
-    # end
-    # within "#flight-#{@flight3.id}" do
-    #   require "pry"; binding.pry
-    #   expect(page).to have_content("Bill Seacaster")
-    # end
   end
 
   it "has button to remove passenger from flight" do

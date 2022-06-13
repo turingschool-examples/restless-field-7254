040b8208522165569c9263f8c57cfc4c546fcc99
Sarah Hall <swatson1994@yahoo.com>
2022-06-13 13:42:59 -0400

    Feat: Add method to sort passengers by amount of flights

--- app/models/airline.rb
+++ app/models/airline.rb
@@ -9,4 +9,12 @@ class Airline < ApplicationRecord
             .distinct
             .order(:name)
   end
+
+  def all_adult_passengers_sorted
+    flights.joins(passengers: :passenger_flights)
+            .where('passengers.age >= ?', 18)
+            .select('passengers.*, count(passenger_flights.passenger_id = passengers.id)')
+            .group('passengers.id')
+            .order(count: :desc, name: :asc)
+  end
 end

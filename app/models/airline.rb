e28cb06a6e82a4d81de025613b2d2bde0f43200e
Zachary Hazelwood <zhaze001@gmail.com>
2022-06-13 12:00:26 -0600

    Test: Extension 1 method, it fails

--- app/models/airline.rb
+++ app/models/airline.rb
@@ -8,4 +8,11 @@ class Airline < ApplicationRecord
     .distinct('passengers.name')
     .pluck('passengers.name')
   end
+
+  def frequent_fliers
+    passengers.select('passengers.name, count(*) as count')
+    .where('passengers.age >= 18')
+    .where('passengers.status = true')
+    .group(count: :desc)
+  end
 end
--- spec/models/airline_spec.rb
+++ spec/models/airline_spec.rb
@@ -16,4 +16,17 @@ RSpec.describe Airline, type: :model do
 
     expect(air1.flyable_adult_names).to eq(["Sally"])
   end
+
+  it "lists frequent fliers" do
+    air1 = Airline.create!(name: "Big Sky")
+    fly1 = air1.flights.create!(number: "177", date: "06/16/22", arrival_city: "Boston", departure_city: "Denver")
+    fly2 = air1.flights.create!(number: "208", date: "06/16/22", arrival_city: "Geneva", departure_city: "Houston")
+    sally = Passenger.create!(name: "Sally", age: 21)
+    sam = Passenger.create!(name: "Sam", age: 18)
+    flypass1 = FlightPassenger.create!(flight_id: fly1.id, passenger_id: sally.id)
+    flypass2 = FlightPassenger.create!(flight_id: fly1.id, passenger_id: sam.id)
+    flypass2 = FlightPassenger.create!(flight_id: fly2.id, passenger_id: sally.id)
+require "pry"; binding.pry
+    expect(air1.frequent_fliers).to eq([sally, sam])
+  end
 end

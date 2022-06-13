b6505b21a2835dca74c5eac563269e6951895154
Cory Bethune <corybbethune@gmail.com>
2022-06-13 10:36:04 -0700

    Update airline model test with associations

--- spec/models/airline_spec.rb
+++ spec/models/airline_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Airline, type: :model do
-  it { should have_many :flights }
+  it { should have_many(:flights) }
+  it { should have_many(:passenger_flights).through(:flights) }
+  it { should have_many(:passengers).through(:passenger_flights) }
 end

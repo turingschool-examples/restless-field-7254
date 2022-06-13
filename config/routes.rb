a4ea158e7d06435e6be433cb32ca697a53a89e70
Saba Bhamidipati <saba.bhamidipati@gmail.com>
2022-06-13 13:21:07 -0400

    test: index page with passenger  names

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/flights', to: 'flights#index'
+  get '/flights/:flight_id/passengers/:passenger_id/destroy', to: 'flight_passengers#destroy'
 end

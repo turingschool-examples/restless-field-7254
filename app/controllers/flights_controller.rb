bef4f2583920249b018a8771c4c0ef763da2c9f9
Eric Espindola <eric_e_2012@hotmail.com>
2022-06-13 10:07:18 -0700

    User story 2

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -2,4 +2,9 @@ class FlightsController < ApplicationController
   def index
     @flights = Flight.all
   end
+
+  def destroy
+  Flight.find(params[:id]).flight_passengers.where("passenger_id = #{params[:passenger_id]}").first.destroy
+  redirect_to '/flights'
+end
 end
--- spec/features/flights/index_spec.rb
+++ spec/features/flights/index_spec.rb
@@ -1,9 +1,5 @@
 require 'rails_helper'
-# As a visitor
-# When I visit the flights index page
-# I see a list of all flight numbers
-# And next to each flight number I see the name of the Airline of that flight
-# And under each flight number I see the names of all that flight's passengers
+
 RSpec.describe 'Flights Index' do
   it 'shows a flights flight number, airline, and passengers' do
     airline1 = Airline.create!(name: 'Frontier')
@@ -37,4 +33,39 @@ RSpec.describe 'Flights Index' do
     end
 
   end
+  # As a visitor
+  # When I visit the flights index page
+  # Next to each passengers name
+  # I see a link or button to remove that passenger from that flight
+  # When I click on that link/button
+  # I'm returned to the flights index page
+  # And I no longer see that passenger listed under that flight
+  it 'can Remove a Passenger from a Flight' do
+    airline1 = Airline.create!(name: 'Frontier')
+    flight1 = airline1.flights.create!(number: '1727', date: '08/03/20', departure_city: 'Denver', arrival_city: 'Reno')
+    passenger1 = Passenger.create(name: 'Eric', age: 28)
+    passenger2 = Passenger.create(name: 'Mike', age: 40)
+    passenger3 = Passenger.create(name: 'Dani', age: 35)
+    FlightPassenger.create(flight_id: flight1.id, passenger_id: passenger1.id)
+    FlightPassenger.create(flight_id: flight1.id, passenger_id: passenger2.id)
+    FlightPassenger.create(flight_id: flight1.id, passenger_id: passenger3.id)
+
+    visit "/flights"
+
+    within "#flight-0" do
+    expect(page).to have_content("Eric")
+    expect(page).to have_content("Mike")
+    expect(page).to have_content("Dani")
+    end
+    click_button "Remove #{passenger2.name} From Flight"
+    expect(current_path).to eq('/flights')
+
+    within "#flight-0" do
+    expect(page).to have_content("Eric")
+    expect(page).to_not have_content("Mike")
+    expect(page).to have_content("Dani")
+    end
+
+  end
+
 end

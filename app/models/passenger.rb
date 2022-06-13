a2918e5531f678156c6693fa1117d8c268e18977
Zachary P <zachary.s.prince@outlook.com>
2022-06-13 13:55:42 -0400

    Add airlines relationship

--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -1,6 +1,7 @@
 class Passenger < ApplicationRecord
   has_many :passenger_flights
   has_many :flights, through: :passenger_flights
+  has_many :airlines, through: :flights
 
   def flight_association(flight_id)
     passenger_flights.select(:id)

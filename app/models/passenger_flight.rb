d92735b3a4a37e1a6f59a9e251dd52e638caed8f
Cory Bethune <corybbethune@gmail.com>
2022-06-13 11:00:48 -0700

    final commit

--- app/models/passenger_flight.rb
+++ app/models/passenger_flight.rb
@@ -3,9 +3,8 @@ class PassengerFlight < ApplicationRecord
   belongs_to :flight
 
   def self.remove_passenger(pass_id, fli_id)
-    all.where(passenger_id: pass_id)
-       .where(flight_id: fli_id)
-       .first
-       .delete
+    where(passenger_id: pass_id, flight_id: fli_id)
+    .delete(1)
+
   end
 end

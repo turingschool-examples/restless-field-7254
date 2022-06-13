d5c453b7e44ddc900a135e58feef295f2ee75713
Caden Jarrett <caden.jarrett99@gmail.com>
2022-06-13 10:38:35 -0600

    Docs: created model method to get airline passengers that are above 18 years old, in a distinct array

--- app/models/airline.rb
+++ app/models/airline.rb
@@ -1,3 +1,8 @@
 class Airline < ApplicationRecord
   has_many :flights
+  has_many :passengers, through: :flights
+
+  def distinct_adult_passengers
+    passengers.where('age >= ?', '18').select('name').distinct.pluck(:name)
+  end
 end

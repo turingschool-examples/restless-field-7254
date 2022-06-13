7e2446a4df7474ba156297b0b3f23023bc421803
Nick Jacobs <nickjacobsatc@gmail.com>
2022-06-13 12:27:33 -0500

    Refactor: destroyed flightpassenger more properly

--- app/controllers/flight_passengers_controller.rb
+++ app/controllers/flight_passengers_controller.rb
@@ -1,8 +1,10 @@
 class FlightPassengersController < ApplicationController
 
   def destroy
+    @flight = Flight.find(params[:flight_id])
     @passenger = Passenger.find(params[:id])
-    flight_passenger = FlightPassenger.destroy(@passenger.flight_passengers.first.id)
+    @flight_passenger = FlightPassenger.find_by(flight_id: @flight.id, passenger_id: @passenger.id)
+    @flight_passenger.destroy
     redirect_to flights_path
   end
 

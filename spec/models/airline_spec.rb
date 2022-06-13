7504adf2e2cf513f0a429300063e015376aa6823
Zachary P <zachary.s.prince@outlook.com>
2022-06-13 12:56:26 -0400

    Add passengers relationship test

--- spec/models/airline_spec.rb
+++ spec/models/airline_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.describe Airline, type: :model do
   describe 'relationships' do
     it { should have_many :flights }
+    it { should have_many :passengers, through: :flights }
   end
   describe 'behaviors' do
     it 'can return all adult passengers' do

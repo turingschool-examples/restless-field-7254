3e445faefd649417f559308d2a17123f2b9a92ff
Stephen Wilkens <stephenwilkens@gmail.com>
2022-06-13 12:00:46 -0600

    Got stuck working through user story 2, started working on user story 3

--- app/models/flight_passenger.rb
+++ app/models/flight_passenger.rb
@@ -1,4 +1,8 @@
 class FlightPassenger < ApplicationRecord
   belongs_to :flight
   belongs_to :passenger
+
+  def self.fp_find
+    where(flight_id: flight.id, passenger_id: self.passenger.id)
+  end
 end

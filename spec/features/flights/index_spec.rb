ceda1a6a4bf8ae6c271c42d74496a06b1a89f650
Sarah Hall <swatson1994@yahoo.com>
2022-06-13 11:43:10 -0400

    Test: Add test for airline name

--- spec/features/flights/index_spec.rb
+++ spec/features/flights/index_spec.rb
@@ -4,10 +4,11 @@ RSpec.describe 'Flights index page', type: :model do
 
   before(:each) do
     @united = Airline.create!(name: "Wind United")
+    @spirit = Airline.create!(name: "Spirit of Freedom")
     @flight1 = @united.flights.create!(number: 879, date: Date.today, departure_city: "Miami", arrival_city: "Portland")
     @flight2 = @united.flights.create!(number: 564, date: Date.today, departure_city: "Fort Myers", arrival_city: "Syracuse")
     @flight3 = @united.flights.create!(number: 231, date: Date.today, departure_city: "Port Charlotte", arrival_city: "Boston")
-    @flight4 = @united.flights.create!(number: 693, date: Date.today, departure_city: "Tampa", arrival_city: "Denver")
+    @flight4 = @spirit.flights.create!(number: 693, date: Date.today, departure_city: "Tampa", arrival_city: "Denver")
   end
 
   it 'has a list of all flight numbers' do
@@ -16,5 +17,11 @@ RSpec.describe 'Flights index page', type: :model do
     expect(page).to have_content(@flight2.number)
     expect(page).to have_content(@flight3.number)
     expect(page).to have_content(@flight4.number)
+  end
+
+  it 'has the airline listed beside the flight number' do
+    visit "/flights"
+    expect(page).to have_content(@spirit.name)
+    expect(page).to have_content(@united.name)
   end
 end

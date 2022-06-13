efdfad2b5ceef883790333c22bb8a984080a3920
Luke Swenson <LukeSwenson95@outlook.com>
2022-06-13 09:07:52 -0600

    Intial commit

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_14_175541) do
+ActiveRecord::Schema.define(version: 2022_06_13_150613) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,8 +29,21 @@ ActiveRecord::Schema.define(version: 2022_01_14_175541) do
     t.bigint "airline_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "passenger_id"
     t.index ["airline_id"], name: "index_flights_on_airline_id"
+    t.index ["passenger_id"], name: "index_flights_on_passenger_id"
+  end
+
+  create_table "passengers", force: :cascade do |t|
+    t.string "name"
+    t.integer "age"
+    t.bigint "flight_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["flight_id"], name: "index_passengers_on_flight_id"
   end
 
   add_foreign_key "flights", "airlines"
+  add_foreign_key "flights", "passengers"
+  add_foreign_key "passengers", "flights"
 end

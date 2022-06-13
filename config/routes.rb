bb017432246d4c9ab15161e3fa827cb26656e1d0
Bryce Wein <bryce.wein@gmail.com>
2022-06-13 12:03:15 -0400

    Feat: U/S 2

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :flights, only: %i[index]
+
+  resources :flights do
+    resources :passengers, only: %i[destroy], controller: 'flight_passengers'
+  end
 end

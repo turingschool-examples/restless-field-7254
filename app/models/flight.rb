7e5473102ab9a88e29ea8fdfb90b6718750e4364
Chris Kjolhede <ckjolhede@gmail.com>
2022-06-13 10:04:36 -0600

    create all table relationships

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,3 +1,5 @@
 class Flight < ApplicationRecord
   belongs_to :airline
+  has_many :flight_passengers
+  has_many :passengers, through: :flight_passengers
 end
--- spec/models/flight_passenger_spec.rb
+++ spec/models/flight_passenger_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
   RSpec.describe FlightPassenger, type: :model do
   describe "relationships" do
-    it { should belong_to :flights}
-    it { should belong_to :passengers}
+    it { should belong_to :flight}
+    it { should belong_to :passenger}
   end
 end

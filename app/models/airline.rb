8932345f719369558d1dea6370d9c1b815d7fe44
Zachary P <zachary.s.prince@outlook.com>
2022-06-13 13:16:03 -0400

    Add airlines adult passenger method

--- app/models/airline.rb
+++ app/models/airline.rb
@@ -1,5 +1,9 @@
 class Airline < ApplicationRecord
   has_many :flights
   has_many :passengers, through: :flights
+  def adult_passengers
+    passengers.where("age > '18'")
+              .distinct('name')
+  end
 end
 

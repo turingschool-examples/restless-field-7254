4fa90a98ed95cc4a97e328a0dee7eee649cd4701
Sage Skaff <skaffsage@gmail.com>
2022-06-13 10:59:18 -0700

    Add remove passenger test

--- spec/features/flights/index_spec.rb
+++ spec/features/flights/index_spec.rb
@@ -52,4 +52,47 @@ RSpec.describe 'Flights Index Page' do
       expect(page).to_not have_content('Stella')
     end
   end
+
+  it 'has button to remove passenger' do
+    airline1 = Airline.create(name: 'Sorth Weast Airlines')
+    airline2 = Airline.create(name: 'Lambda Airlines')
+
+    flight1 = airline1.flights.create(number: '9372', date: '01/01/22', departure_city: 'Denver',
+                                      arrival_city: 'London')
+    flight2 = airline1.flights.create(number: '8612', date: '02/02/22', departure_city: 'Denver',
+                                      arrival_city: 'Madrid')
+    flight3 = airline2.flights.create(number: '1739', date: '03/03/22', departure_city: 'Denver',
+                                      arrival_city: 'Paris')
+
+    passenger1 = Passenger.create(name: 'Stella', age: 31)
+    passenger2 = Passenger.create(name: 'Astra', age: 16)
+    passenger3 = Passenger.create(name: 'Casseopia', age: 42)
+    passenger4 = Passenger.create(name: 'Andromeda', age: 27)
+
+    FlightPassenger.create(flight: flight1, passenger: passenger1)
+    FlightPassenger.create(flight: flight1, passenger: passenger2)
+    FlightPassenger.create(flight: flight2, passenger: passenger3)
+    FlightPassenger.create(flight: flight3, passenger: passenger4)
+
+    visit '/flights'
+
+    within '#index-0' do
+      expect(page).to have_content('Stella')
+      expect(page).to have_content('Astra')
+      click_button 'Remove Passenger'
+      expect(page).to_not have_content('Stella')
+    end
+
+    within '#index-1' do
+      expect(page).to have_content('Casseopia')
+      click_button 'Remove Passenger'
+      expect(page).to_not have_content('Casseopia')
+    end
+
+    within '#index-2' do
+      expect(page).to have_content('Andromeda')
+      click_button 'Remove Passenger'
+      expect(page).to_not have_content('Andromeda')
+    end
+  end
 end

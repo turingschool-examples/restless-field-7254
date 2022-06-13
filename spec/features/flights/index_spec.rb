10440213374f378cf3c6466fe5e054aa45bf8409
Zachary Hazelwood <zhaze001@gmail.com>
2022-06-13 10:07:36 -0600

    Feat: Flight Index displays info

--- spec/features/flights/index_spec.rb
+++ spec/features/flights/index_spec.rb
@@ -4,12 +4,12 @@ RSpec.describe 'Flights Index Page', type: :feature do
   describe "User Story 1" do
     it "displays all Flights, and their airline" do
       air1 = Airline.create!(name: "Big Sky")
-      fly1 = air1.flight.create!(number: "177", date: "06/16/22", arrival_city: "Boston", departure_city: "Denver")
-      fly2 = air1.flight.create!(number: "208", date: "06/16/22", arrival_city: "Geneva", departure_city: "Houston")
+      fly1 = air1.flights.create!(number: "177", date: "06/16/22", arrival_city: "Boston", departure_city: "Denver")
+      fly2 = air1.flights.create!(number: "208", date: "06/16/22", arrival_city: "Geneva", departure_city: "Houston")
 
       air2 = Airline.create!(name: "Denta")
-      fly3 = air2.flight.create!(number: "444", date: "06/16/22", arrival_city: "Langston", departure_city: "Tampa")
-      fly4 = air2.flight.create!(number: "101", date: "06/16/22", arrival_city: "Tampa", departure_city: "Santa Fe")
+      fly3 = air2.flights.create!(number: "444", date: "06/16/22", arrival_city: "Langston", departure_city: "Tampa")
+      fly4 = air2.flights.create!(number: "101", date: "06/16/22", arrival_city: "Tampa", departure_city: "Santa Fe")
 
       sally = Passenger.create!(name: "Sally")
       sam = Passenger.create!(name: "Sam")
@@ -30,7 +30,6 @@ RSpec.describe 'Flights Index Page', type: :feature do
       flypass8 = FlightPassenger.create!(flight_id: fly4.id, passenger_id: laura.id)
 
       visit flights_path
-
       within ".all_flights" do
         expect(page).to have_content("Flight: 177")
         expect(page).to have_content("Flight: 208")

e199720b2fceaaa4858320a3a44872930ec85770
Zachary P <zachary.s.prince@outlook.com>
2022-06-13 11:10:27 -0400

    Add passenger and passenger flight migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_14_175541) do
+ActiveRecord::Schema.define(version: 2022_06_13_150922) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,5 +32,20 @@ ActiveRecord::Schema.define(version: 2022_01_14_175541) do
     t.index ["airline_id"], name: "index_flights_on_airline_id"
   end
 
+  create_table "passenger_flights", force: :cascade do |t|
+    t.bigint "flight_id"
+    t.bigint "passenger_id"
+    t.index ["flight_id"], name: "index_passenger_flights_on_flight_id"
+    t.index ["passenger_id"], name: "index_passenger_flights_on_passenger_id"
+  end
+
+  create_table "passengers", force: :cascade do |t|
+    t.string "name"
+    t.string "age"
+    t.string "integer"
+  end
+
   add_foreign_key "flights", "airlines"
+  add_foreign_key "passenger_flights", "flights"
+  add_foreign_key "passenger_flights", "passengers"
 end

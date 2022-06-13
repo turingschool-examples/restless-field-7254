143f4081ab00de685cbae7477bfe93eb101a01f2
Cory Bethune <corybbethune@gmail.com>
2022-06-13 08:48:41 -0700

    Update spec and data now loads correctly. Create route to flight index.

--- spec/features/flights/index_spec.rb
+++ spec/features/flights/index_spec.rb
@@ -2,16 +2,16 @@ require 'rails_helper'
 
 RSpec.describe "flight index" do
   before(:each) do
-    @airline1 = Airline.create!(Name: "American")
-    @airline2 = Airline.create!(Name: "Delta")
-    @airline3 = Airline.create!(Name: "JetBlue")
-    @airline4 = Airline.create!(Name: "SouthWest")
-    @airline5 = Airline.create!(Name: "United")
+    @airline1 = Airline.create!(name: "American")
+    @airline2 = Airline.create!(name: "Delta")
+    # @airline3 = Airline.create!(Name: "JetBlue")
+    # @airline4 = Airline.create!(Name: "SouthWest")
+    # @airline5 = Airline.create!(Name: "United")
 
-    @flight1 = @airline1.flights.create!(Number: "7990", Date: "2/7/2022", Departure City: "Glendale", Arrival City: "Dallas")
-    @flight2 = @airline1.flights.create!(Number: "3940", Date: "3/10/2022", Departure City: "Detroit", Arrival City: "Orlando")
-    @flight3 = @airline2.flights.create!(Number: "9091", Date: "5/17/2022", Departure City: "Chicago", Arrival City: "Kansas City")
-    @flight4 = @airline2.flights.create!(Number: "6798", Date: "10/9/2022", Departure City: "Tulsa", Arrival City: "Fresno")
+    @flight1 = @airline1.flights.create!(number: "7990", date: "2/7/2022", departure_city: "Glendale", arrival_city: "Dallas")
+    @flight2 = @airline1.flights.create!(number: "3940", date: "3/10/2022", departure_city: "Detroit", arrival_city: "Orlando")
+    @flight3 = @airline2.flights.create!(number: "9091", date: "5/17/2022", departure_city: "Chicago", arrival_city: "Kansas City")
+    @flight4 = @airline2.flights.create!(number: "6798", date: "10/9/2022", departure_city: "Tulsa", arrival_city: "Fresno")
 
     @passenger1 = Passenger.create!(name: 'Cory', age: 32)
     @passenger2 = Passenger.create!(name: 'Bill', age: 27)
@@ -29,9 +29,13 @@ RSpec.describe "flight index" do
 
     within("#flight-#{@flight1.id}") do
       expect(page).to have_content(@flight1.number)
-      expect(page).to have_content(@flight1.airline.name)
+      expect(page).to have_content(@airline1.name)
       expect(page).to have_content(@passenger1.name)
       expect(page).to have_content(@passenger2.name)
+      expect(page).to_not have_content(@passenger3.name)
+      expect(page).to_not have_content(@passenger4.name)
+      expect(page).to_not have_content(@airline2.name)
+      expect(page).to_not have_content(@flight2.number)
     end
   end
 end

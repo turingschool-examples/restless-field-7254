b09c7a21d733269698034ea52182b4dc881c4b99
Chris Kjolhede <ckjolhede@gmail.com>
2022-06-13 11:27:07 -0600

    Create Flight Index content with remove buttons

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 
-resources :flights, only: :index
-resources :airlines, only: :show
+  resources :flights, only: :index
+  resources :airlines, only: :show
+  delete '/flights/:flight_id/passengers/:passenger_id', to: 'flight_passengers#destroy'
 end
--- spec/features/flights/index_spec.rb
+++ spec/features/flights/index_spec.rb
@@ -3,10 +3,10 @@ require 'rails_helper'
 RSpec.describe Flight, type: :feature do
   describe 'visitors on flight index' do
     before(:each) do
-      @delta = Airline.create!(name: 'delta')
+      @delta = Airline.create!(name: 'Delta')
       @flight_1 = @delta.flights.create!(number: '1', date: '1/1/22', departure_city: 'Denver', arrival_city: 'DC')
-      @steve = Passenger.create!(name: 'steve', age: 29)
-      @frank = Passenger.create!(name: 'frank', age: 27)
+      @steve = Passenger.create!(name: 'Steve', age: 29)
+      @frank = Passenger.create!(name: 'Frank', age: 27)
       @flight_pass_1 = FlightPassenger.create!(flight_id: @flight_1.id, passenger_id: @steve.id)
       @flight_pass_2 = FlightPassenger.create!(flight_id: @flight_1.id, passenger_id: @frank.id)
       @delta = Airline.create!(name: 'Delta')
@@ -20,19 +20,26 @@ RSpec.describe Flight, type: :feature do
     end
  
     it 'displays all flight, their airline, and their passenger list' do
-      expect(page).to have_content("Flight: #{@flight_1.number} Airline: #{@delta.name}")
-      expect(page).to have_content("Passengers: #{@steve.name} #{@frank.name}")
-      expect(page).to have_content("Flight: #{@flight_2.number} Airline: #{@delta.name}")
-      expect(page).to have_content("Passengers: #{@jordan.name} #{@jimmy.name}")
+        save_and_open_page
+      within("#flight-#{@flight_1.id}") do
+        expect(page).to have_content("Flight: 1 Airline: Delta")
+        expect(page).to have_content("Steve")
+        expect(page).to have_content("Frank")
+      end 
+      within("#flight-#{@flight_2.id}") do
+        expect(page).to have_content("Flight: 2 Airline: Delta")
+        expect(page).to have_content("Jimmy")
+        expect(page).to have_content("Jordan")
+      end
     end
 
     it "next to each passenger on the page is a button that will remove the passenger from the flight when clicked but not delete the customer record" do
-      within("#passenger-#{@steve.id}") do
-        expect(page).to have_button("BuhBye #{@steve.name}")
-          click_button("BuhBye #{@steve.name}")
+        expect(page).to have_button("BuhBye Steve")
+          click_button("BuhBye Steve")
+
         expect(current_path).to eq("/flights")
         expect(page).to_not have_content(@steve.name)
-      end
+      
     end
   end
 end

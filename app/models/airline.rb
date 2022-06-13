2507cd6e0c2e687f050df1b298e46d73ea9ad136
Stirling Hostetter <stirhoss@icloud.com>
2022-06-13 12:45:41 -0500

    Add method to order the list of passengers

--- app/models/airline.rb
+++ app/models/airline.rb
@@ -3,6 +3,13 @@ class Airline < ApplicationRecord
   has_many :passengers, through: :flights
 
   def airline_adult_passengers
-    passengers.where('age >= 18').distinct
+    passengers.where('age >= 18')
+              .distinct
+  end
+
+  def airline_adult_passengers_ordered
+    passengers.where('age >= 18')
+              .group(:id)
+              .order('count(flights.id) DESC')
   end
 end

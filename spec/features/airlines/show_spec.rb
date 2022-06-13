b179152a60e31c1ec7c823bb7e9291e273662e03
Jim Riddle <jimriddle0@gmail.com>
2022-06-13 11:10:08 -0500

    wrote test for user story 3

---
+++ spec/features/airlines/show_spec.rb
@@ -0,0 +1,48 @@
+require 'rails_helper'
+
+RSpec.describe Airline, type: :feature do
+  describe 'the index page' do
+
+    it 'tests user story 3' do
+      # As a visitor
+      # When I visit an airline's show page
+      # Then I see a list of passengers that have flights on that airline
+      # And I see that this list is unique (no duplicate passengers)
+      # And I see that this list only includes adult passengers
+      #
+      # (Note: an adult is anyone with age greater than or equal to 18)
+
+
+      airline_1 = Airline.create!(name: "Amercian")
+      flight_1 = airline_1.flights.create!(number: 7990, date: 2/7/2022, departure_city: "Glendale", arrival_city: "Dallas")
+      flight_2 = airline_1.flights.create!(number: 3940, date: 3/10/2022, departure_city: "Detroit", arrival_city: "Orlando")
+      flight_3 = airline_1.flights.create!(number: 9091, date: 5/17/2022, departure_city: "Chicago", arrival_city: "Kansas City")
+      flight_4 = airline_1.flights.create!(number: 6798, date: 10/9/2022, departure_city: "Tulsa", arrival_city: "Fresno")
+
+      passenger_1 = Passenger.create!(name:"Brennan", age: 40)
+      passenger_2 = Passenger.create!(name:"Aabria", age: 24)
+      passenger_3 = Passenger.create!(name:"Bill", age: 15)
+      passenger_4 = Passenger.create!(name:"Misty", age: 12)
+      passenger_5 = Passenger.create!(name:"Kingston", age: 29)
+
+      PassengerFlight.create!(passenger_id: passenger_1.id, flight_id: flight_1.id)
+      PassengerFlight.create!(passenger_id: passenger_1.id, flight_id: flight_2.id)
+      PassengerFlight.create!(passenger_id: passenger_2.id, flight_id: flight_3.id)
+      PassengerFlight.create!(passenger_id: passenger_3.id, flight_id: flight_4.id)
+      PassengerFlight.create!(passenger_id: passenger_4.id, flight_id: flight_1.id)
+      PassengerFlight.create!(passenger_id: passenger_5.id, flight_id: flight_4.id)
+
+
+      visit "/airlines/#{airline_1.id}"
+
+      expect(page).to have_content(passenger_1.name, count:1)
+      expect(page).to have_content(passenger_2.name, count:1)
+      expect(page).to have_content(passenger_5.name, count:1)
+      expect(page).to_not have_content(passenger_3.name, count:1)
+      expect(page).to_not have_content(passenger_4.name, count:1)
+
+
+    end
+
+  end
+end

6cf968678c56173504fcef8ec6a2252869b5e6b2
Casey Fazio <98674727+casefaz@users.noreply.github.com>
2022-06-13 11:29:04 -0600

    All user stories fully functioning

--- app/controllers/flight_passengers_controller.rb
+++ app/controllers/flight_passengers_controller.rb
@@ -1,8 +1,8 @@
 class FlightPassengersController < ApplicationController
   def destroy
-    flight = Flight.find(params[:flight_id])
-    passenger = Passenger.find(params[:id])
-    FlightPassenger.destroy(flight: flight, passenger: passenger)
+    # flight = Flight.find(params[:flight_id])
+    passenger = FlightPassenger.find_by(passenger: params[:id], flight: params[:flight_id])
+    passenger.destroy
     redirect_to flights_path
   end
 end

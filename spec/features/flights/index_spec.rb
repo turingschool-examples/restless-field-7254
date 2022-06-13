9b4266addcc78333011c80cd40bfd149ef6485be
Matt Deming <vballmatt44@gmail.com>
2022-06-13 09:45:03 -0600

    Add test for user story 1

---
+++ spec/features/flights/index_spec.rb
@@ -0,0 +1,33 @@
+require "rails_helper"
+
+RSpec.describe "flights index page" do
+  it "shows all flight numbers, name of the airline, and the names of all passengers for that flight" do
+    frontier = Airline.create!(name: "Frontier")
+    southwest = Airline.create!(name: "Southwest")
+    flight1 = frontier.flights.create!(number: "F234", date: "03/24/2022", departure_city: "Denver", arrival_city: "Los Angeles")
+    flight2 = frontier.flights.create!(number: "F345", date: "03/25/2022", departure_city: "Denver", arrival_city: "Seattle")
+    flight3 = frontier.flights.create!(number: "S456", date: "03/24/2022", departure_city: "Denver", arrival_city: "New Orleans")
+    flight4 = frontier.flights.create!(number: "S567", date: "03/25/2022", departure_city: "Denver", arrival_city: "Dallas")
+    passenger1 = flight1.passengers.create!(name: "Sarah Micheals", age: 22)
+    passenger2 = flight1.passengers.create!(name: "Micheal Stevens", age: 34)
+    passenger3 = flight2.passengers.create!(name: "Ray Charles", age: 67)
+    passenger4 = flight2.passengers.create!(name: "Bernie Mac", age: 59)
+    passenger5 = flight3.passengers.create!(name: "Shaq O'Niel", age: 50)
+    passenger6 = flight3.passengers.create!(name: "Gary Smarts", age: 76)
+    passenger7 = flight4.passengers.create!(name: "Flynn Rider", age: 24)
+    passenger8 = flight4.passengers.create!(name: "Rapunzel", age: 23)
+
+    visit "/flights"
+    expect(page).to have_content("Flight: F234")
+    expect(page).to have_content("Flight: F345")
+    expect(page).to have_content("Flight: S456")
+    expect(page).to have_content("Flight: S567")
+    expect(page).to have_content("Frontier")
+    expect(page).to have_content("Southwest")
+    expect(page).to have_content("Sarah Micheals")
+    expect(page).to have_content("Ray Charles")
+    expect(page).to have_content("Gary Smarts")
+    expect(page).to have_content("Flynn Rider")
+
+  end
+end

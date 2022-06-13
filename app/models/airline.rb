ddb8a2e6e4475424954f9df64a80c10338fc3c0b
Jim Riddle <jimriddle0@gmail.com>
2022-06-13 11:19:48 -0500

    modified model view and controllers to pass user story 3

--- app/models/airline.rb
+++ app/models/airline.rb
@@ -1,4 +1,8 @@
 class Airline < ApplicationRecord
   has_many :flights
   has_many :passengers, through: :flights
+
+  def adult_passengers
+    passengers.where("age >= 18").distinct
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+  get '/airlines/:id', to: 'airlines#show'
 
   get '/flights', to: 'flights#index'
   delete '/flights/:f_id/passengers/:p_id', to: 'passenger_flights#destroy'

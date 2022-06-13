900bf4ccd76555adbddbd3ab7185f6253826e20e
Zachary P <zachary.s.prince@outlook.com>
2022-06-13 13:53:56 -0400

    Add frequesnt_adult_flyers method

--- app/models/airline.rb
+++ app/models/airline.rb
@@ -1,8 +1,18 @@
 class Airline < ApplicationRecord
   has_many :flights
   has_many :passengers, through: :flights
+
   def adult_passengers
     passengers.where("age > '18'")
+              .distinct('name')
+  end
+
+  def frequent_adult_flyers
+    passengers.select('passengers.*, count(passenger_flights) total_passenger_flights_count')
+              .where("age > '18'")
+              .joins(:airlines)
+              .group('passengers.id')
+              .order(total_passenger_flights_count: :desc)
               .distinct('name')
   end
 end

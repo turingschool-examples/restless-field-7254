198ebd4bcc1090863da593c5c780b9f33dd6d316
Andrew King <dev.andrewking@gmail.com>
2022-06-13 11:27:15 -0500

    add route, add show action

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -2,4 +2,8 @@ class FlightsController < ApplicationController
   def index
     @flights = Flight.all
   end
+
+  def show
+    @flight = Flight.find(params[:flight_id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get "/flights", to: "flights#index"
+  get "/flights/:flight_id", to: "flights#show"
   delete "/flights/:flight_id/passengers/:passenger_id", to: "flight_passengers#destroy"
 end

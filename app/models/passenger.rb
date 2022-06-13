b0fb1111261828f4a5882fe84b75377bc7ce4056
Zachary P <zachary.s.prince@outlook.com>
2022-06-13 12:40:18 -0400

    Add flight_association method

--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -1,4 +1,11 @@
 class Passenger < ApplicationRecord
   has_many :passenger_flights
   has_many :flights, through: :passenger_flights
+
+  def flight_association(flight_id)
+    passenger_flights.select(:id)
+                     .where(flight_id: flight_id)
+                     .last
+                     .id
+  end
 end

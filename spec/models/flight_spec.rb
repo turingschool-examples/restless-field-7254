fd91b44d022def17190716d08fce66f11db352cc
Zachary Hazelwood <zhaze001@gmail.com>
2022-06-13 11:19:54 -0600

    Test: #flyable_adults

--- spec/models/flight_spec.rb
+++ spec/models/flight_spec.rb
@@ -26,4 +26,15 @@ RSpec.describe Flight, type: :model do
 
     expect(fly1.flyable_passengers).to eq([sally])
   end
+
+  it "show flyable adults" do
+    air1 = Airline.create!(name: "Big Sky")
+    fly1 = air1.flights.create!(number: "177", date: "06/16/22", arrival_city: "Boston", departure_city: "Denver")
+    sally = Passenger.create!(name: "Sally", age: 21)
+    sam = Passenger.create!(name: "Sam", age: 10)
+    flypass1 = FlightPassenger.create!(flight_id: fly1.id, passenger_id: sally.id)
+    flypass2 = FlightPassenger.create!(flight_id: fly1.id, passenger_id: sam.id)
+
+    expect(fly1.flyable_adults).to eq([sally])
+  end
 end

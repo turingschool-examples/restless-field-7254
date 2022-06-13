bb0c4bb2c8314aee1d54746dcbaec7cbcfa22d7a
Andrew King <dev.andrewking@gmail.com>
2022-06-13 12:20:13 -0500

    add frequent flyers

--- app/models/airline.rb
+++ app/models/airline.rb
@@ -4,6 +4,14 @@ class Airline < ApplicationRecord
 
 
   def only_adults
-    passengers.where('passengers.age >= 18')
+    passengers.where('passengers.age >= 18').distinct
+  end
+
+  def frequent_flyers
+    passengers.joins(:flights)
+              .where('passengers.age >= 18')
+              .select('passengers.*, count(passengers.id) AS passenger_flights')
+              .group('passengers.id')
+              .order(passenger_flights: :desc)
   end
 end

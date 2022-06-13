19de2ed5dddae059159d78879dbc6a5ffd27a4e5
Cory Bethune <corybbethune@gmail.com>
2022-06-13 08:25:26 -0700

    Add associations to models. Test pass

--- spec/models/passenger_flight_spec.rb
+++ spec/models/passenger_flight_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe PassengerFlight, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "associations" do
+    it { should belong_to :flight}
+    it { should belong_to :passenger}
+  end
 end
--- spec/models/passenger_spec.rb
+++ spec/models/passenger_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Passenger, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'associations' do
+    it { should have_many(:passenger_flights)}
+    it { should have_many(:flights).through(:passenger_flights)}
+  end
 end

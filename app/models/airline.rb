6a57ac11eb1a66bf0dfd7355163d1ea08c716f2d
Eric Espindola <eric_e_2012@hotmail.com>
2022-06-13 10:39:42 -0700

    user story 3

--- app/models/airline.rb
+++ app/models/airline.rb
@@ -1,3 +1,10 @@
 class Airline < ApplicationRecord
   has_many :flights
+  has_many :passengers, through: :flights
+
+  def airline_passengers
+    wip = self.passengers
+    .where("passengers.age >= 18")
+    .distinct
+  end
 end
